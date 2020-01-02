33a1d0ce2f5fa6ae93450ac88984c67f3efedf00
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-01-02 22:10:16 +0900

    クラスに定義していた処理を外部関数に切り出し

--- todolist/src/main/kotlin/TaskController.kt
+++ todolist/src/main/kotlin/TaskController.kt
@@ -13,12 +13,7 @@ class TaskController(private val objectMapper: ObjectMapper,
     }
 
     fun create(): Route = Route { req, res ->
-        val request: TaskCreateRequest =
-            try {
-                objectMapper.readValue(req.bodyAsBytes(), TaskCreateRequest::class.java)
-            } catch (e: Exception) {
-                throw halt(400)
-            }
+        val request: TaskCreateRequest = objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400)
         val task = taskRepository.create(request.content)
         res.status(201)
         task
