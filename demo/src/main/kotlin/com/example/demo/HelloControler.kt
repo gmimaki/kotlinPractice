4648dec9d8525c3e2184875a2dc6fa733b2df12b
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-01-05 12:08:53 +0900

    日本語でこんにちはの挨拶ができるように

--- demo/src/main/kotlin/com/example/demo/HelloControler.kt
+++ demo/src/main/kotlin/com/example/demo/HelloControler.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloControler {
+class HelloController(private val greeter: Greeter) {
     @GetMapping("hello")
-    fun hello(@RequestParam("name") name: String): String = "Hello $name"
+    fun hello(@RequestParam("name") name: String): String = greeter.hello(name)
 }
--- demo/src/main/kotlin/com/example/demo/JapaneseGreeter.kt
+++ demo/src/main/kotlin/com/example/demo/JapaneseGreeter.kt
@@ -1,4 +1,8 @@
 package com.example.demo
 
-class JapaneseGreeter {
+import org.springframework.stereotype.Component
+
+@Component
+class JapaneseGreeter : Greeter {
+    override fun hello(name: String): String = "こんにちは, $name!"
 }
